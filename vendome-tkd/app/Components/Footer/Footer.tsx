222a8418231b3bc08008e9c0f4ccde58755730e9
alexandre-joliet <alexandre.joliet@gmail.com>
2024-03-12 15:07:44 +0100

    :sparkles: Add footer component + style

---
+++ vendome-tkd/app/Components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import styles from "./page.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.infos_container}>
+        <p className={styles.infos_title}> Taekwondo Club Vendôme</p>
+        <p className={styles.infos_address}>
+          6 Rue Geoffroy Martel, 41100 Vendôme
+        </p>
+      </div>
+      <a
+        href="https://www.facebook.com/groups/2491193871168095?locale=fr_FR"
+        className={styles.footer_social}
+      >
+        <img
+          src="/logos/facebook.png"
+          alt="Logo Facebook"
+          className={styles.social_fb}
+        />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
