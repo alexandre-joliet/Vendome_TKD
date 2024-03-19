b6ad2288c3d99f8640ea9445e9488d8e176f9938
alexandre-joliet <alexandre.joliet@gmail.com>
2024-03-19 17:03:08 +0100

    :construction: Update infos layout + WIP carrousel

--- app/infos/page.tsx
+++ app/infos/page.tsx
@@ -26,29 +26,134 @@ const Infos = () => {
 
           <section className={styles.article_section}>
             <h2 className={styles.section_title}>
-              <img
-                src="/icons/schedule_black_24dp.svg"
-                alt=""
-                aria-hidden="true"
-              />
-              Horaires des entraînements
+              <img src="/icons/info_black_24dp.svg" alt="" aria-hidden="true" />
+              Horaires et tarifs
             </h2>
-            <p className={styles.section_text}>
-              Les entrainements ont lieu tous les <b>Mardis et Jeudis</b> aux
-              horaires suivantes :
-            </p>
-            <table className={styles.table_hours}>
-              <tbody>
-                <tr>
-                  <td>Enfants (-13 ans)</td>
-                  <td className={styles.hours}>18h - 19h</td>
-                </tr>
-                <tr>
-                  <td>Adolescents & Adultes</td>
-                  <td className={styles.hours}>19h05 - 20h30</td>
-                </tr>
-              </tbody>
-            </table>
+            <div className={styles.card_carrousel}>
+              <div className={`${styles.card_container} ${styles.children}`}>
+                <div className={styles.card_info}>
+                  <h3 className={styles.info_title}>Enfants (-13 ans)</h3>
+                  <p className={styles.info_price}>
+                    <span className={styles.price}>120€</span>/ pour l'année
+                  </p>
+                </div>
+                <div className={styles.card_hours}>
+                  <p className={styles.info_hours}>
+                    <img
+                      src="/icons/event_white_48dp.svg"
+                      alt=""
+                      aria-hidden="true"
+                    />
+                    Mardis et jeudis 18h - 19h
+                  </p>
+                </div>
+                <div className={styles.card_extras}>
+                  <table className={styles.table_price}>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <span>&#11088;</span>Licence FFTDA (obligatoire)
+                        </td>
+                        <td className={styles.extra_price}>+35€</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <span>&#11088;</span>Passeport (obligatoire 1ère année
+                          et valable 8 ans)
+                        </td>
+                        <td className={styles.extra_price}>+20€</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <p className={styles.card_free}>
+                    Deux cours d'essai gratuits et sans engagement !
+                  </p>
+                </div>
+              </div>
+
+              <div className={`${styles.card_container} ${styles.teens}`}>
+                <div className={styles.card_info}>
+                  <h3 className={styles.info_title}>Adolescents</h3>
+                  <p className={styles.info_price}>
+                    <span className={styles.price}>130€</span>/ pour l'année
+                  </p>
+                </div>
+                <div className={styles.card_hours}>
+                  <p className={styles.info_hours}>
+                    <img
+                      src="/icons/event_white_48dp.svg"
+                      alt=""
+                      aria-hidden="true"
+                    />
+                    Mardis et jeudis 18h - 19h
+                  </p>
+                </div>
+                <div className={styles.card_extras}>
+                  <table className={styles.table_price}>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <span>&#11088;</span>Licence FFTDA (obligatoire)
+                        </td>
+                        <td className={styles.extra_price}>+35€</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <span>&#11088;</span>Passeport (obligatoire 1ère année
+                          et valable 8 ans)
+                        </td>
+                        <td className={styles.extra_price}>+20€</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <p className={styles.card_free}>
+                    Deux cours d'essai gratuits et sans engagement !
+                  </p>
+                </div>
+              </div>
+
+              <div className={`${styles.card_container} ${styles.adults}`}>
+                <div className={styles.card_info}>
+                  <h3 className={styles.info_title}>Adultes</h3>
+                  <p className={styles.info_price}>
+                    <span className={styles.price}>150€</span>/ pour l'année
+                  </p>
+                </div>
+                <div className={styles.card_hours}>
+                  <p className={styles.info_hours}>
+                    <img
+                      src="/icons/event_white_48dp.svg"
+                      alt=""
+                      aria-hidden="true"
+                    />
+                    Mardis et jeudis 19h05 - 20h30
+                  </p>
+                </div>
+                <div className={styles.card_extras}>
+                  <table className={styles.table_price}>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <span>&#11088;</span>Licence FFTDA (obligatoire)
+                        </td>
+                        <td className={styles.extra_price}>+35€</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <span>&#11088;</span>Passeport (obligatoire 1ère année
+                          et valable 8 ans)
+                        </td>
+                        <td className={styles.extra_price}>+20€</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <p className={styles.card_free}>
+                    Deux cours d'essai gratuits et sans engagement !
+                  </p>
+                </div>
+              </div>
+            </div>
+
             <h4 className={styles.section_note}>
               &#128161; Vacances scolaires :
             </h4>
@@ -56,37 +161,6 @@ const Infos = () => {
               Sauf mention contraire du professeur, aucun cours n'a lieu durant
               les vacances scolaires.
             </p>
-          </section>
-
-          <section className={styles.article_section}>
-            <h2 className={styles.section_title}>
-              <img src="/icons/euro_black_24dp.svg" alt="" aria-hidden="true" />
-              Tarifs
-            </h2>
-            <table className={styles.table_price}>
-              <tbody>
-                <tr>
-                  <td className={styles.table_row}>Enfants (-13 ans)</td>
-                  <td className={styles.price}>120€</td>
-                </tr>
-                <tr>
-                  <td>Adolescents (13-17 ans)</td>
-                  <td className={styles.price}>130€</td>
-                </tr>
-                <tr>
-                  <td>Adultes</td>
-                  <td className={styles.price}>150€</td>
-                </tr>
-                <tr>
-                  <td>Licence (obligatoire)</td>
-                  <td className={styles.price}>35€</td>
-                </tr>
-                <tr>
-                  <td>Passeport (achat unique)</td>
-                  <td className={styles.price}>20€</td>
-                </tr>
-              </tbody>
-            </table>
             <h4 className={styles.section_note}>
               &#128161; Note sur l'équipement :
             </h4>
@@ -103,41 +177,43 @@ const Infos = () => {
             </p>
           </section>
 
-          <section className={styles.article_section}>
-            <h2 className={styles.section_title}>
-              <img
-                src="/icons/location_on_black_24dp.svg"
-                alt=""
-                aria-hidden="true"
-              />
-              Nous trouver
-            </h2>
-            <p className={styles.location_text}>
-              Tous les entrainements ont lieu au{" "}
-              <b>Complexe Sportif d'Arts Martiaux</b> de Vendôme situé au :
-            </p>
-            <p className={styles.location_address}>
-              6 Rue Geoffroy Martel, 41100 Vendôme
-            </p>
-            <p className={styles.location_text}>
-              Un parking est à disposition juste à côté du complexe pour
-              stationner.
-            </p>
+          <div className={styles.bg_wrapper}>
+            <section className={styles.article_section}>
+              <h2 className={styles.section_title}>
+                <img
+                  src="/icons/location_on_black_24dp.svg"
+                  alt=""
+                  aria-hidden="true"
+                />
+                Nous trouver
+              </h2>
+              <p className={styles.location_text}>
+                Tous les entrainements ont lieu au{" "}
+                <b>Complexe Sportif d'Arts Martiaux</b> de Vendôme situé au :
+              </p>
+              <p className={styles.location_address}>
+                6 Rue Geoffroy Martel, 41100 Vendôme
+              </p>
+              <p className={styles.location_text}>
+                Un parking est à disposition juste à côté du complexe pour
+                stationner.
+              </p>
 
-            <iframe
-              src="https://www.openstreetmap.org/export/embed.html?bbox=1.0679805278778078%2C47.78808242879076%2C1.0789024829864504%2C47.79619128338885&amp;layer=mapnik&amp;marker=47.79213701425164%2C1.073441505432129"
-              width={MapSize.width}
-              height="300"
-              style={{ border: 0, borderRadius: "10px" }}
-              className={styles.map_iframe}
-            ></iframe>
-            {/* <br />
+              <iframe
+                src="https://www.openstreetmap.org/export/embed.html?bbox=1.0679805278778078%2C47.78808242879076%2C1.0789024829864504%2C47.79619128338885&amp;layer=mapnik&amp;marker=47.79213701425164%2C1.073441505432129"
+                width={MapSize.width}
+                height="300"
+                style={{ border: 0, borderRadius: "10px" }}
+                className={styles.map_iframe}
+              ></iframe>
+              {/* <br />
             <small>
               <a href="https://www.openstreetmap.org/?mlat=47.79214&amp;mlon=1.07344#map=17/47.79214/1.07344">
                 Afficher une carte plus grande
               </a>
             </small> */}
-          </section>
+            </section>
+          </div>
         </article>
       </main>
       <Footer />
