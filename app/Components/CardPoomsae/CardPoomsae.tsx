85296fe7d6c18287e54c9cba3c2d80ce7bd2f299
alexandre-joliet <alexandre.joliet@gmail.com>
2024-03-22 17:29:57 +0100

    :construction: WIP component + style

---
+++ app/Components/CardPoomsae/CardPoomsae.tsx
@@ -0,0 +1,78 @@
+"use client";
+import styles from "./page.module.css";
+import { PoomsaeType } from "@/app/@types/poomsae";
+import { useState } from "react";
+
+type PoomsaeProps = {
+  poomsae: PoomsaeType;
+};
+
+const CardPoomsae = ({ poomsae }: PoomsaeProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpandCard = () => {
+    if (isExpanded) {
+      setIsExpanded(false);
+    } else {
+      setIsExpanded(true);
+    }
+  };
+
+  const expandClass = isExpanded ? `${styles.expanded}` : "";
+
+  const bgPoomsae = () => {
+    let className = "";
+    const poomsaeId = poomsae.id;
+
+    if (poomsaeId === 1 || poomsaeId === 2) {
+      className = `${styles.bg_poomsaeyellow}`;
+    } else if (poomsaeId === 3 || poomsaeId === 4 || poomsaeId === 5) {
+      className = `${styles.bg_poomsaeblue}`;
+    } else {
+      className = `${styles.bg_poomsaered}`;
+    }
+    return className;
+  };
+
+  return (
+    <>
+      <section className={styles.section_poomsae}>
+        <div
+          className={`${styles.name_container} ${bgPoomsae()}`}
+          onClick={handleExpandCard}
+        >
+          <h4 className={styles.poomsae_name}>
+            {poomsae.id} - {poomsae.name}
+          </h4>
+          <button className={styles.expand_toggler}>
+            <img
+              src="/icons/expand_more_black_24dp.svg"
+              alt="Dérouler le texte"
+            />
+          </button>
+        </div>
+        <div className={`${styles.content_container} ${expandClass}`}>
+          <p className={styles.poomsae_description}>{poomsae.description}</p>
+          <img src={poomsae.picture} alt="" />
+          <h5 className={styles.poomsae_ressources}>Ressources</h5>
+          <ul className={styles.poomsae_list}>
+            <li>
+              <span className={styles.emoji}>&#128218;</span>
+              <a href={poomsae.file_pdf} target="_blank">
+                Télécharger la fiche PDF
+              </a>
+            </li>
+            <li>
+              <span className={styles.emoji}>&#127909;</span>
+              <a href={poomsae.video} target="_blank">
+                Continuer vers la vidéo Youtube (en anglais)
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CardPoomsae;
