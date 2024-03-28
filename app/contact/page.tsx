4545007fa13dbb22032811650537bcac4e0db30f
alexandre-joliet <alexandre.joliet@gmail.com>
2024-03-28 16:56:52 +0100

    :truck: Move form into a component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,19 +1,9 @@
-"use client";
-import { FormEvent, useState } from "react";
+import ContactForm from "../Components/ContactForm/ContactForm";
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
 import styles from "./page.module.css";
 
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [mail, setMail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleFormSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(name, mail, message);
-  };
-
   return (
     <>
       <Header />
@@ -28,48 +18,7 @@ const Contact = () => {
               ci-dessous. Nous vous répondrons dès que possible !
             </p>{" "}
             <h2 className={styles.section_title}>Formulaire de contact</h2>
-            <form onSubmit={handleFormSubmit} className={styles.form}>
-              <label htmlFor="name" id="name" className={styles.form_label}>
-                Votre nom{" "}
-                <span className={styles.label_required}>(requis)</span>
-              </label>
-              <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-                className={styles.form_input}
-              />
-              <label htmlFor="mail" id="mail" className={styles.form_label}>
-                Votre adresse mail{" "}
-                <span className={styles.label_required}>(requis)</span>
-              </label>
-              <input
-                type="mail"
-                id="mail"
-                value={mail}
-                onChange={(event) => setMail(event.target.value)}
-                className={styles.form_input}
-              />
-              <label
-                htmlFor="message"
-                id="message"
-                className={styles.form_label}
-              >
-                Votre message{" "}
-                <span className={styles.label_required}>(requis)</span>
-              </label>
-              <textarea
-                id="message"
-                rows={10}
-                value={message}
-                onChange={(event) => setMessage(event.target.value)}
-                className={styles.form_text}
-              />
-              <button type="submit" className={styles.form_button}>
-                Envoyer
-              </button>
-            </form>
+            <ContactForm />
           </section>
         </article>
       </main>
