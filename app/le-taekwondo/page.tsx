a6104c7b0527cb717bded7559df9a1c255fdec00
alexandre-joliet <alexandre.joliet@gmail.com>
2024-04-04 16:21:14 +0200

    :iphone: Update responsive

--- app/le-taekwondo/page.tsx
+++ app/le-taekwondo/page.tsx
@@ -12,13 +12,18 @@ const LeTaekwondo = () => {
         </section>
         <article className={styles.main_article}>
           <section className={styles.article_section}>
-            <p className={styles.section_text}>
-              Le Taekwondo, un art martial originaire de Corée, incarne une
-              fusion harmonieuse entre la force brute et la discipline mentale.
-            </p>
             <blockquote className={styles.section_quote}>
+              <img
+                src="/icons/martial_arts.svg"
+                alt=""
+                className={styles.quote_icon}
+              />
               <strong>« La voie des pieds et des poings »</strong>
             </blockquote>
+            <p className={styles.section_text}>
+              Le Taekwondo, art martial originaire de Corée, incarne une fusion
+              harmonieuse entre la force brute et la discipline mentale.
+            </p>
             <p className={styles.section_text}>
               Avec ses racines historiques profondes remontant à des
               millénaires, le Taekwondo a évolué pour devenir bien plus qu'un
@@ -33,69 +38,92 @@ const LeTaekwondo = () => {
 
           <section className={styles.article_section}>
             <h2 className={styles.section_title}>L'Histoire du Taekwondo</h2>
-            <img
-              src="/images/TKDH-H.jpg"
-              alt="Photographie d'une vieille estampe montrant deux personnages s'affrontant au milieu de la foule"
-              className={styles.section_image}
-            />
-            <p className={styles.section_text}>
-              Le Taekwondo puise ses racines dans une longue tradition de combat
-              et de philosophie. Ses origines remontent à l'ère des Trois
-              Royaumes de la Corée (57 av. JC - 668 ap. JC), où les techniques
-              de combat étaient développées pour la défense personnelle et la
-              protection des royaumes.
-            </p>
-            <p className={styles.section_text}>
+            <div className={styles.section_subcontainer}>
+              <figure className={styles.figure_left}>
+                <img
+                  src="/images/TKDH-H copie.webp"
+                  alt="Photographie d'une vieille estampe montrant deux personnages s'affrontant au milieu de la foule"
+                  className={styles.section_image}
+                />
+              </figure>
+              <p className={styles.section_text}>
+                Le Taekwondo puise ses racines dans une longue tradition de
+                combat et de philosophie. Ses origines remontent à l'ère des
+                Trois Royaumes de la Corée (57 av. JC - 668 ap. JC), où les
+                techniques de combat étaient développées pour la défense
+                personnelle et la protection des royaumes. Cependant, le
+                Taekwondo moderne prend véritablement forme après la Seconde
+                Guerre mondiale, lorsque divers styles de combats traditionnels
+                coréens sont codifiés et standardisés en un système unique, sous
+                l'impulsion du gouvernement coréen désireux de promouvoir un art
+                martial national.
+              </p>
+            </div>
+            {/* <p className={styles.section_text}>
               Cependant, le Taekwondo moderne prend véritablement forme après la
               Seconde Guerre mondiale, lorsque divers styles de combats
               traditionnels coréens sont codifiés et standardisés en un système
               unique, sous l'impulsion du gouvernement coréen désireux de
               promouvoir un art martial national.
-            </p>
-            <p className={styles.section_text}>
-              Le véritable tournant pour le Taekwondo survient en 1955, lorsque
-              la fédération coréenne de Taekwondo (KTA) est fondée. Peu de temps
-              après, en 1966, la fédération internationale de Taekwondo (ITF)
-              voit le jour grâce au Général Choi Hong Hi, ouvrant la voie à la
-              reconnaissance mondiale de cet art martial.
-            </p>
-            <figure>
-              <img
-                src="/images/TKDH-Choi.jpg"
-                alt="Photographie d'époque montrant 3 pratiquants de Taekwondo effectuant un coup de pied latéral"
-                className={styles.section_image}
-              />
-              <figcaption className={styles.img_caption}>
-                Le Général Choi (au centre) et deux autres pratiquants durant
-                une démonstration
-              </figcaption>
-            </figure>
-            <p className={styles.section_text}>
+            </p> */}
+
+            <div className={styles.section_subcontainer}>
+              <figure className={styles.figure_right}>
+                <img
+                  src="/images/TKDH-Choi copie.webp"
+                  alt="Photographie d'époque montrant 2 pratiquants de Taekwondo effectuant un coup de pied latéral bloqués par un troisième personnage central"
+                  className={styles.section_image}
+                />
+                <figcaption className={styles.img_caption}>
+                  Le Général Choi (au centre) et deux autres pratiquants durant
+                  une démonstration
+                </figcaption>
+              </figure>
+              <p className={styles.section_text}>
+                Le véritable tournant pour le Taekwondo survient en 1955,
+                lorsque la fédération coréenne de Taekwondo (KTA) est fondée.
+                Peu de temps après, en 1966, la fédération internationale de
+                Taekwondo (ITF) voit le jour grâce au Général Choi Hong Hi,
+                ouvrant la voie à la reconnaissance mondiale de cet art martial.
+                Toutefois, des différends internes conduisent à une scission en
+                1972, donnant naissance à une deuxième organisation majeure, la
+                World Taekwondo Federation (renommée WT depuis peu). Cette
+                division marque le début d'une évolution divergente entre les
+                deux organisations, avec des variations dans les règles, les
+                techniques et les compétitions. <br /> <br />
+                &#128161; <b>NB : </b>Dans notre club, nous enseignons le
+                Taekwondo WT.
+              </p>
+
+              {/* <p className={styles.section_text}>
               Toutefois, des différends internes conduisent à une scission en
               1972, donnant naissance à une deuxième organisation majeure, la
               World Taekwondo Federation (renommée WT depuis peu). Cette
               division marque le début d'une évolution divergente entre les deux
               organisations, avec des variations dans les règles, les techniques
               et les compétitions.
-            </p>
-            <p className={styles.section_text}>
-              &#128161; <b>NB : </b>Dans notre club, nous enseignons le
-              Taekwondo WT.
-            </p>
-            <p className={styles.section_text}>
-              Depuis lors, le Taekwondo s'est répandu à travers le monde,
-              devenant l'un des sports de combat les plus populaires aux Jeux
-              olympiques en 2000. Sa popularité repose non seulement sur son
-              efficacité en tant qu'art de self-défense, mais aussi sur les
-              valeurs qu'il promeut : discipline, respect, persévérance et
-              contrôle de soi. Aujourd'hui, le Taekwondo est pratiqué par des
-              millions de personnes de tous âges et de tous horizons, incarnant
-              à la fois un héritage culturel riche et une discipline physique et
-              mentale exigeante.
-            </p>
-            <figure>
+            </p> */}
+              {/* <p className={styles.section_text}>
+                &#128161; <b>NB : </b>Dans notre club, nous enseignons le
+                Taekwondo WT.
+              </p> */}
+            </div>
+            <div className={styles.section_subcontainer}>
+              <p className={styles.section_text}>
+                Depuis lors, le Taekwondo s'est répandu à travers le monde,
+                devenant l'un des sports de combat les plus populaires aux Jeux
+                olympiques en 2000. Sa popularité repose non seulement sur son
+                efficacité en tant qu'art de self-défense, mais aussi sur les
+                valeurs qu'il promeut : discipline, respect, persévérance et
+                contrôle de soi. Aujourd'hui, le Taekwondo est pratiqué par des
+                millions de personnes de tous âges et de tous horizons,
+                incarnant à la fois un héritage culturel riche et une discipline
+                physique et mentale exigeante.
+              </p>
+            </div>
+            <figure className={styles.figure_center}>
               <img
-                src="/images/TKDH-M.jpg"
+                src="/images/TKDH-M copie.webp"
                 alt="Photographie moderne montrant deux combattants lors des JO de 2016.3 L'un des combattants porte un coup de pied retourné à la tête de son adversaire"
                 className={styles.section_image}
               />
