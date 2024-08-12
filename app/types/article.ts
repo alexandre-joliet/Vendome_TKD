4b2435bc19b85d3132123945e7c080d07757d09e
alexandre-joliet <alexandre.joliet@gmail.com>
2024-08-12 17:28:29 +0200

    :label: Add type for news

---
+++ app/types/article.ts
@@ -0,0 +1,10 @@
+export type ArticleType = {
+  id: number;
+    title: string;
+    date: string;
+    textCard: string;
+    textFull: string;
+    image: string;
+    altIMage: string;
+    creditsImage: string;
+}
